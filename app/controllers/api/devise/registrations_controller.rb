0b87e2c6faa72d86cab27628fb8bfab7c04190e5
NightWolf007 <dimon19960121@gmail.com>
2015-10-07 20:08:52 +0300

    some fixes

--- app/controllers/api/devise/registrations_controller.rb
+++ app/controllers/api/devise/registrations_controller.rb
@@ -1,17 +1,2 @@
 class Api::Devise::RegistrationsController < Devise::RegistrationsController
-  before_filter :authenticate_user_from_token!
-
-  private
-
-    def authenticate_user_from_token!
-      authenticate_with_http_token do |token, options|
-        user_email = options[:user_email].presence
-        user = user_email && User.find_by_email(user_email)
-        token.sub! 'token=', ''
-        token.sub! 'email=', 'user_email'
-        if user && Devise.secure_compare(user.authentication_token, token)
-          sign_in user, store: false
-        end
-      end
-    end
 end
