19ed33560182aa8b2508f93f950c2dd02d37c2f4
NightWolf007 <dimon19960121@gmail.com>
2015-10-12 15:10:46 +0300

    Added uploading avatars

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
       resources :comments
       resources :tags
       resources :users
+
+      post 'images' => 'images#create'
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
