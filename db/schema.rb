7f3456b302184c629225dcb80d40422db527af4d
NightWolf007 <dimon19960121@gmail.com>
2015-10-13 23:14:37 +0300

    Deleting :date column from posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151012123448) do
+ActiveRecord::Schema.define(version: 20151013193434) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "message",    limit: 65535
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20151012123448) do
     t.string   "title",      limit: 255
     t.integer  "user_id",    limit: 4
     t.text     "text",       limit: 65535
-    t.datetime "date"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.string   "image",      limit: 255
